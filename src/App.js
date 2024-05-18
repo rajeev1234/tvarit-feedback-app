b76d3d831cad490b095634baeb71ba0047391e07
rajeevduality <rajeev@duality.company>
2024-05-19 02:35:35 +0545

    url update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link , Routes} from 'react-router-dom';
+import FeedbackFormPage from './pages/FeedbackFormPage';
+import FeedbackListPage from './pages/FeedbackListPage';
+import './styles/styles.css';
 
-function App() {
+const App = () => {
+  const [feedbackList, setFeedbackList] = useState([]);
+
+  const handleSubmitFeedback = (feedback) => {
+    setFeedbackList([...feedbackList, feedback]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">FeedbackApp</Link>
+          <div className="navbar-links">
+            <Link to="/" className="navbar-item">Submit Feedback</Link>
+            <Link to="/feedback-list" className="navbar-item">Feedback List</Link>
+          </div>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" exact element={<FeedbackFormPage onSubmit={handleSubmitFeedback} />}></Route>
+        <Route path="/feedback-list" element={<FeedbackListPage feedbackList={feedbackList} />} ></Route>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
